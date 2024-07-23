053eceded5fc7d20f4258102b0b1c06135a8a020
Serkan YILMAZ <serkan.yilmaz@tubitak.gov.tr>
2024-07-23 16:14:17 +0300

    Login page initialized

---
+++ ypbs-fe/app/login/page.tsx
@@ -0,0 +1,15 @@
+export default function main (){
+
+    return(
+        <>
+            <div style={{display : "flex",
+                flexDirection: "column"
+            }}>
+                <input placeholder="Username"></input>
+                <input placeholder="Password"></input>
+                <button>Enter</button>
+            </div>
+            
+        </>
+    )
+}

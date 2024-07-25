c7bfad35d97f0b211f0ad3635515444f2bf86785
Serdar İhsan ÇETİNTAŞ <serdar.cetintas@tubitak.gov.tr>
2024-07-25 09:51:54 +0300

    born-today, lib klasörüne taşındı api-helper silindi.

--- ypbs-fe/app/api_helper/URLs.tsx
+++
@@ -1,9 +0,0 @@
-// Update this
-export const COMMON_PATH = "localhost:8080/";
-
-export const LOGIN_PATH = (username: string, password: string) =>
-  "api/users/login" + "?username=" + username + "&password=" + password;
-
-export const BORN_TODAY_PATH = () => "api/users/born-today";
-
-export const CUSTOM_INPUT = () => "api/dummy/dummy";

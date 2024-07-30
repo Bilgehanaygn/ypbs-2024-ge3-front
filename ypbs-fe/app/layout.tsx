8cec1fcdbd7b62982687fc940e567c42ce999b3c
bilgehan.aygun <bilgehan.aygun@tubitak.gov.tr>
2024-07-30 11:49:01 +0300

    YPBS-250 strict mode false

--- ypbs-fe/app/layout.tsx
+++ ypbs-fe/app/layout.tsx
@@ -1,58 +1,48 @@
 "use client";
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {createTheme} from "@/lib/theme/theme";
-import {ThemeProvider} from "@mui/material/styles";
-import {NavigationBar} from "../lib/navigation-bar/NavigationBar";
-import {usePathname, useRouter} from 'next/navigation';
-import {userInterface, UserContext} from "@/lib/context/UserContext";
-import {useContext, useEffect, useState} from "react";
+import { createTheme } from "@/lib/theme/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { NavigationBar } from "../lib/navigation-bar/NavigationBar";
+import { usePathname, useRouter } from "next/navigation";
+import { userInterface, UserContext } from "@/lib/context/UserContext";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme = createTheme();
+  const pathName = usePathname();
+  const isLoginPage = pathName === "/login";
 
-export default function RootLayout({children,}: { children: React.ReactNode; }) {
-    const theme = createTheme();
-    const pathName = usePathname();
-    const isLoginPage = pathName === "/login";
+  const [userState, setUserState] = useState<userInterface | null>(null);
+  const router = useRouter();
 
-    const [userState,setUserState] = useState<userInterface | null>(null);
-    const router = useRouter();
-
-    useEffect(() => {
-      const fetchData = async () => {
-          try {
-              console.log(userState);
-              const response = await axios.get("api/userHeader");
-              setUserState(response.data);
-              console.log(userState);
-              if (userState === null) {
-                  router.push("/login");
-              }
-          } catch (error) {
-              console.error("Error fetching user data:", error);
-              router.push("/login");
-          }
-      };
-      fetchData();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("/api/userHeader");
+      if (response.status !== 200) return;
+      setUserState(response.data);
+    };
+    fetchData();
   }, []);
 
-    
-    return (
-        <html lang="en">
-        <body className={inter.className}>
-        <UserContext.Provider value={[userState,setUserState]}>
-            <ThemeProvider theme={theme}>
-                {!isLoginPage && <NavigationBar/>}
-                {!isLoginPage && <div style={{marginTop: 0, height: 65}}></div>}
-                <div style={{height: 500}}>
-                    {children}
-                </div>
-            </ThemeProvider>
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <UserContext.Provider value={[userState, setUserState]}>
+          <ThemeProvider theme={theme}>
+            {!isLoginPage && <NavigationBar />}
+            {!isLoginPage && <div style={{ marginTop: 0, height: 65 }}></div>}
+            <div style={{ height: 500 }}>{children}</div>
+          </ThemeProvider>
         </UserContext.Provider>
-        </body>
-        </html>
-
-    );
+      </body>
+    </html>
+  );
 }
--- ypbs-fe/next.config.mjs
+++ ypbs-fe/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8080/api/:path*' // Proxy to Backend
-        }
-      ]
-    }
-  };
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
+  reactStrictMode: false,
+};
 export default nextConfig;

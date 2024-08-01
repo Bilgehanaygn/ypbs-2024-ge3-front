8a228ea3e87de7d4354dddab401c074f65414dd9
aybars.aksoy <aybars.aksoy@tubitak.gov.tr>
2024-08-01 15:43:01 +0300

    YPBS-255 Haberler Componenti oluşturuldu.

--- lib/home-page/HomePage.tsx
+++ lib/home-page/HomePage.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeProvider } from "@mui/material/styles";
 import { Box } from "@mui/material";
+import News from "@/lib/news/News";
 
 
 
@@ -9,7 +10,7 @@ export default function HomePage() {
   
   return (
     <div className="anaSayfaDiv">
-        Ana Sayfa
+        <News/>
     </div>
   );
 }

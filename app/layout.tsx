b5e1cf6f583730fa999cc0814e3745abdfef6a67
Ali Buğra ŞAHIN <alibugra.sahin@tubitak.gov.tr>
2024-08-12 15:27:32 +0300

    YPBS-365 Gelen kullancıyı login olduktan sonra geldiği endpoint'e pushlanıyor. Eğer kullanıcı login değilse otomatik login page'e atılıyor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,62 @@
 "use client";
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
-import { createTheme } from "@/lib/theme/theme";
-import { ThemeProvider } from "@mui/material/styles";
-import { NavigationBar } from "../lib/navigation-bar/NavigationBar";
-import { usePathname, useRouter } from "next/navigation";
+import {createTheme} from "@/lib/theme/theme";
+import {ThemeProvider} from "@mui/material/styles";
+import {NavigationBar} from "../lib/navigation-bar/NavigationBar";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
 import axios from "axios";
 import LoadingPage from "@/lib/loading/LoadingPage";
 import useSWR from "swr";
-import { UserInterface } from "@/lib/user/user";
+import {UserInterface} from "@/lib/user/user";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  const theme = createTheme();
-  const pathName = usePathname();
+    const theme = createTheme();
+    const pathName = usePathname();
+    const router = useRouter();
 
-  const { data: userState, isLoading } = useSWR(
-    "/api/user/userHeader",
-    fetchUserData,
-    {
-      revalidateOnFocus: false,
-      shouldRetryOnError: false,
-    }
-  );
-
-  async function fetchUserData() {
-    const response = await axios.get<UserInterface>("/api/user/userHeader");
-    return response.data;
-  }
-
-  if (isLoading) {
-    return (
-      <html lang="en">
-        <body className={inter.className}>
-          <LoadingPage />
-        </body>
-      </html>
+    const {data: userState, isLoading} = useSWR(
+        "/api/user/userHeader",
+        fetchUserData,
+        {
+            revalidateOnFocus: false,
+            shouldRetryOnError: false,
+        }
     );
-  }
 
-  return (
-    <html lang="en">
-      <body className={inter.className}>
+    async function fetchUserData() {
+        const response = await axios.get<UserInterface>("/api/user/userHeader");
+
+        if(!response.data) router.push("/?navigateTo=" + pathName.slice(1));
+
+        return response.data;
+    }
+
+    if (isLoading) {
+        return (
+            <html lang="en">
+            <body className={inter.className}>
+            <LoadingPage/>
+            </body>
+            </html>
+        );
+    }
+
+    return (
+        <html lang="en">
+        <body className={inter.className}>
         <ThemeProvider theme={theme}>
-          {userState ? <NavigationBar /> : null}
-          {userState && <div style={{ marginTop: 0, height: 65 }}></div>}
-          {(pathName === "/" || userState) && <div>{children}</div>}
+            {userState ? <NavigationBar/> : null}
+            {userState && <div style={{marginTop: 0, height: 65}}></div>}
+            {(pathName === "/" || userState) && <div>{children}</div>}
         </ThemeProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
